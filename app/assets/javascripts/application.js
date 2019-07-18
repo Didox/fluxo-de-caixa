1426cbd8e12a155dee30b7aa439e48075754b4a1
Danilo <didox_59@yahoo.com.br>
2019-07-18 12:12:49 -0300

    outros tipos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= requite jquery-3.4.1.min
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
