516fe5f11e516699381628989782cc14214f414b
Danilo <didox_59@yahoo.com.br>
2019-07-24 13:45:15 -0300

    pdf

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -1,7 +1,7 @@
 class Pedido < ApplicationRecord
   validates :placa, presence: true
 
-  default_scope { order(vencimento: :desc) }
+  default_scope { order("numero_fatura desc, vencimento desc") }
   
   before_validation :valida_placa
 
